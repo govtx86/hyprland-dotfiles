e4e7ce91ef29f16193bf985fac3350c70bdb121b
govtx86 <govindsanal08@gmail.com>
2025-08-01 19:53:09 +0530

    Update neovim config

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+local opt = vim.opt
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.tabstop = 4
+opt.softtabstop = 4
